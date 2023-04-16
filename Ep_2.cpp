3d11891a3fb68ccfaca67524988a84f8a1101f68
amirhossein amiran <93470832+amirhosseiamiran2004@users.noreply.github.com>
2023-04-16 09:27:35 +0330

    what is a const Syntax
    in this Ep we learn what is const in C++ and worked with them.

---
+++ Ep_2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(){
+	// what is a const Syntax
+	// calculate the circumference of the circle	
+	const double PI = 3.14159;
+	double radius = 10;
+	double circumference = 2 * PI * radius;
+	std::cout << circumference << "cm";
+	
+	// Light speed
+	const int LIGHT_SPEED = 299792458;
+	// Screen Resolution
+	const int WIDTH = 1920;
+	const int HEIGHT = 1080; 
+	
+	return 0;
+}

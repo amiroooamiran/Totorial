7d1fe9351f46c4b204dacc25eb6f2180b06f3f78
amirhossein amiran <93470832+amirhosseiamiran2004@users.noreply.github.com>
2023-04-16 08:57:08 +0330

    Ep_1 from C++ Tutorial
    in this Ep learn [ whole number, number including decimal, single character, Boolean, strings.

---
+++ Ep_1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+
+int main()
+{
+    //integer (whole number)
+    int age = 21;
+    int year = 2023;
+    int days = 3;
+    
+    //double (number including decimal)
+    double price = 10.99;
+    double gpa = 2.5;
+
+    // single charecter
+    char grade = 'A';
+    char intitale = 'B';
+    char dollerSing = '$';
+
+    // boolean (true or false)
+    bool student = false;
+    bool power = true;
+
+    // string (object that represtnts a sequence of text)
+    std::string name = "Amiroo";
+    std::string day = "thrsday";
+
+    std::cout << "Hello " << name << " you are " << age << " yers old";
+
+    return 0;
+}
